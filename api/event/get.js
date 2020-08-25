ce6854bb143ff9c0cafb0dfd800dd5499666d4d4
eckhardo <1af112AB>
2020-08-25 13:56:08 +0200

    Add Secondary Index for Event with status and endingDate

---
+++ api/event/get.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const databaseManager = require('../dynamoDbConnect');
+
+const TABLE_NAME = process.env.CONFIG_USER_TABLE;
+const dynamoDb = databaseManager.connectDynamoDB(TABLE_NAME);
+
+
+const get = async (event) => {
+    console.log('getEndedAuctions::start');
+    const now = new Date();
+    const params = {
+        TableName: TABLE_NAME,
+        IndexName: 'statusEndingDate-Index',
+        KeyConditionExpression: '#status =:status AND endingDate >= :now',
+        ExpressionAttributeValues: {':status' : 'OPEN', ':now' : now.toISOString()},
+        ExpressionAttributeNames: {'#status' : 'status'}
+    }
+
+    const result = await dynamoDb.query(params).promise();
+    console.log('getEndedAuctions::result: ', result);
+    return result.Items;
+
+}
+module.exports = {
+    getEndedEvents: get
+}
